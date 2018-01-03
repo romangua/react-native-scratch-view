890217be74077c1a54ba3c6805e50ac298dab294
Román Guarino <romanguarino@hotmail.com>
2018-01-03 17:33:43 -0300

    1

--- src/ScratchImageView.ios.js
+++ src/ScratchImageView.ios.js
@@ -4,26 +4,14 @@ import { NativeModules, requireNativeComponent, View, Text } from 'react-native'
 class ScratchImageView extends Component {
     constructor(props) {
         super(props);
-        //this._onChange = this._onChange.bind(this);
     }
 
-  /*  _onChange(event) {
-        /*if(!this.props.onDateChange) {
-            return;
-        }
-        this.props.onDateChange(event.nativeEvent); */
-    }*/
-
     render() {
-        return <RNScratchImageView {...this.props}/>;
+        return <RNScratchImageView {...this.props} />;
     }
 }
 
 ScratchImageView.propTypes = {
-  /*  day: PropTypes.number,
-    month: PropTypes.number,
-    year: PropTypes.number,
-    onDateChange: PropTypes.func,*/
     ...View.propTypes,
 }
 
