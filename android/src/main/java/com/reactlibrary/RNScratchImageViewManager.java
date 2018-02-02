09ab2fcd710b016a3180ffed7e405aadea389614
Román Guarino <romanguarino@hotmail.com>
2018-02-02 15:45:58 -0300

    s

--- android/src/main/java/com/reactlibrary/RNScratchImageViewManager.java
+++ android/src/main/java/com/reactlibrary/RNScratchImageViewManager.java
@@ -1,38 +1,96 @@
 package com.reactlibrary;
 
 import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
 import com.cooltechworks.views.ScratchImageView;
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.events.EventDispatcher;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+import java.util.Map;
+
+import static java.security.AccessController.getContext;
 
 
 public class RNScratchImageViewManager extends SimpleViewManager<ScratchImageView> {
 
     public static final String REACT_CLASS = "RNScratchImageView";
+    public static final int ON_REVEAL = 1;
 
     @Override
     public String getName() {
         return REACT_CLASS;
     }
 
-    @Override
+   @Override
     public ScratchImageView createViewInstance(ThemedReactContext context) {
         ScratchImageView scratchImageView = new ScratchImageView(context);
         scratchImageView.setBackgroundColor(Color.WHITE);
         scratchImageView.setImageResource(R.drawable.homer);
 
-        scratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
-            @Override
-            public void onRevealed(ScratchImageView tv) {
-                // on reveal
-            }
-
-            @Override
-            public void onRevealPercentChangedListener(ScratchImageView siv, float percent) {
-                // on image percent reveal
-            }
-        });
-
         return scratchImageView;
     }
+
+    @Override
+    protected void addEventEmitters(final ThemedReactContext reactContext, final ScratchImageView view) {
+
+        view.setRevealListener(new ScratchImageView.IRevealListener() {
+            @Override
+            public void onRevealed(com.cooltechworks.views.ScratchImageView scratchImageView) {
+                Log.d("llog", "OnReveal fired ");
+            }
+
+            @Override
+            public void onRevealPercentChangedListener(com.cooltechworks.views.ScratchImageView scratchImageView, float value) {
+                // Value between 0-1
+                Log.d("llog", "onRevealPercentChanged fired: " + value*100 + "%");
+                if(reactContext!=null){
+                    Log.d("llog", "entrooo fired: " + value*100 + "%");
+                    WritableMap event = Arguments.createMap();
+
+                    event.putDouble("percent", value*100);
+                    reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                            view.getId(),
+                            "onRevealPercentChanged",
+                            event);
+                }
+            }
+        });
+    }
+
+  /*  @Override
+    public Map<String,Integer> getCommandsMap() {
+        Log.d("React"," View manager getCommandsMap:");
+        return MapBuilder.of(
+                "onRevealPercentChanged",
+                ON_REVEAL);
+    }
+
+    @Override
+    public void receiveCommand(ScratchImageView view, int commandType, @Nullable ReadableArray args) {
+        Assertions.assertNotNull(view);
+        Assertions.assertNotNull(args);
+        switch (commandType) {
+            case ON_REVEAL: {
+                Log.d("onReveal", "AWSD");
+                return;
+            }
+
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "Unsupported command %d received by %s.",
+                        commandType,
+                        getClass().getSimpleName()));
+        }
+    }*/
 }
--- src/ScratchImageView.android.js
+++ src/ScratchImageView.android.js
@@ -2,20 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import { NativeModules, requireNativeComponent, View, Text } from 'react-native';
 
 class ScratchImageView extends Component {
+
     constructor(props) {
         super(props);
-        this._onChange = this._onChange.bind(this);
+        this.onRevealPercentChanged = this.onRevealPercentChanged.bind(this);
     }
 
-    _onChange(event) {
-        /*if(!this.props.onDateChange) {
+    onRevealPercentChanged(e) {
+        console.log("onRevealPercentChanged: " + e);
+        if(!this.props.onRevealPercentChanged) {
             return;
         }
-        this.props.onDateChange(event.nativeEvent); */
+        this.props.onRevealPercentChanged(event.nativeEvent);
     }
 
     render() {
-        return <RNScratchImageView {...this.props} onChange={this._onChange} />;
+        return 
+            <RNScratchImageView {...this.props}
+                onRevealPercentChanged={this.onRevealPercentChanged}
+            />;
     }
 }
 
@@ -24,12 +29,13 @@ ScratchImageView.propTypes = {
     month: PropTypes.number,
     year: PropTypes.number,
     onDateChange: PropTypes.func,*/
+    onRevealPercentChanged: PropTypes.func,
     ...View.propTypes,
 }
 
 const RNScratchImageView = requireNativeComponent(`RNScratchImageView`, ScratchImageView, {
     nativeOnly: {
-        onChange: true,
+        onRevealPercentChanged: false,
     }
 });
 
