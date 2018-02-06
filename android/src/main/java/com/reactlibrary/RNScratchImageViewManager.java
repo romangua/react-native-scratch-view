05a6af36cfd8183fcd98b40ad1405edcd8ad4314
Román Guarino <romanguarino@hotmail.com>
2018-02-06 12:34:33 -0300

    1

--- android/src/main/java/com/reactlibrary/RNScratchImageViewManager.java
+++ android/src/main/java/com/reactlibrary/RNScratchImageViewManager.java
@@ -1,17 +1,20 @@
 package com.reactlibrary;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import com.cooltechworks.views.ScratchImageView;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
+import com.squareup.picasso.Picasso;
 
 import java.util.Map;
 
@@ -21,6 +24,7 @@ public class RNScratchImageViewManager extends SimpleViewManager<ScratchImageVie
     public static final String REACT_CLASS = "RNScratchImageView";
     private static final String ON_REVEAL_PERCENT_CHANGED = "ON_REVEAL_PERCENT_CHANGED";
     private static final String ON_REVEALED = "ON_REVEALED";
+    private Context _context;
 
     @Override
     public String getName() {
@@ -29,7 +33,8 @@ public class RNScratchImageViewManager extends SimpleViewManager<ScratchImageVie
 
    @Override
     public ScratchImageView createViewInstance(ThemedReactContext context) {
-        ScratchImageView scratchImageView = new ScratchImageView(context);
+        this._context = context;
+        ScratchImageView scratchImageView = new ScratchImageView(_context);
         scratchImageView.setBackgroundColor(Color.WHITE);
         scratchImageView.setImageResource(R.drawable.homer);
 
@@ -41,10 +46,20 @@ public class RNScratchImageViewManager extends SimpleViewManager<ScratchImageVie
         view.setStrokeWidth(value);
     }
 
-    @ReactProp(name = "src")
-    public void setHeight(ScratchImageView view, int value) {
-        view.setMinimumHeight(value);
-        view.setMaxHeight(value);
+    @ReactProp(name = "imageScratched")
+    public void setImageScratched(ScratchImageView view, ReadableMap value) {
+
+        Picasso.with(_context)
+                .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GzFEIXWOydx7khmtlP2_L-geboBoduazwhqO0h2eL856Pdd9")
+               // .resize(50, 50)
+                .centerCrop()
+                .into(view);
+    }
+
+    @ReactProp(name = "imagePattern")
+    public void setImagePattern(ScratchImageView view, ReadableMap value) {
+        // view.setMinimumHeight(value);
+        // view.setMaxHeight(value);
     }
 
    public Map getExportedCustomBubblingEventTypeConstants() {
@@ -88,30 +103,4 @@ public class RNScratchImageViewManager extends SimpleViewManager<ScratchImageVie
             }
         });
     }
-
-  /*  @Override
-    public Map<String,Integer> getCommandsMap() {
-        Log.d("React"," View manager getCommandsMap:");
-        return MapBuilder.of(
-                "onRevealPercentChanged",
-                ON_REVEAL);
-    }
-
-    @Override
-    public void receiveCommand(ScratchImageView view, int commandType, @Nullable ReadableArray args) {
-        Assertions.assertNotNull(view);
-        Assertions.assertNotNull(args);
-        switch (commandType) {
-            case ON_REVEAL: {
-                Log.d("onReveal", "AWSD");
-                return;
-            }
-
-            default:
-                throw new IllegalArgumentException(String.format(
-                        "Unsupported command %d received by %s.",
-                        commandType,
-                        getClass().getSimpleName()));
-        }
-    }*/
 }
